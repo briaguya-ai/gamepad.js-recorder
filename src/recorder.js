eec5b4dd7645c11fb588f897c07cfa723ab4c70f
briaguya <briaguya@alice>
2022-03-06 09:29:12 -0500

    kinda hacky but the n64 svgs are showing up now

--- src/recorder.js
+++ src/recorder.js
@@ -15,23 +15,16 @@ const data = {
 const startRecordingButton = document.querySelector(".btn-primary");
 
 const buttons = [
-  "button_1",
-  "button_2",
-  "button_3",
-  "button_4",
-  "shoulder_top_left",
-  "shoulder_top_right",
-  "shoulder_bottom_left",
-  "shoulder_bottom_right",
-  "select",
-  "start",
-  "stick_button_left",
-  "stick_button_right",
-  "d_pad_up",
-  "d_pad_down",
-  "d_pad_left",
-  "d_pad_right",
-  "vendor"
+  "button_a",
+  "button_b",
+  "button_start",
+  "button_z",
+  "button_l",
+  "button_r",
+  "button_c_up",
+  "button_c_down",
+  "button_c_left",
+  "button_c_right",
 ];
 
 const getGamepad = () =>
@@ -40,9 +33,9 @@ const getGamepad = () =>
 const getGamepadButtons = () => getGamepad().buttons;
 
 startRecordingButton.addEventListener("click", () => {
-  const highlights = [].slice.call(
-    document.querySelectorAll('svg [id$="_highlight"]')
-  );
+  // const buttonElements = [].slice.call(
+  //   document.querySelectorAll('img')
+  // );
 
   let buttonInterval;
 
@@ -51,14 +44,14 @@ startRecordingButton.addEventListener("click", () => {
   let currentButtonIndex = 0;
 
   document
-    .querySelector(`svg [id$="${buttons[currentButtonIndex]}_highlight"]`)
+    .querySelector(`#${buttons[currentButtonIndex]}`)
     .setAttribute("class", "visible");
 
   const waitForInput = () => {
     const gamepad = getGamepad();
 
     document
-      .querySelector(`svg [id$="${buttons[currentButtonIndex]}_highlight"]`)
+      .querySelector(`#${buttons[currentButtonIndex]}`)
       .setAttribute("class", "visible");
 
     if (gamepad && !buttonInterval) {
@@ -76,7 +69,7 @@ startRecordingButton.addEventListener("click", () => {
         if (currentButtonIndex < buttons.length - 1) {
           currentButtonIndex += 1;
 
-          highlights.map((highlight) => highlight.removeAttribute("class"));
+          document.querySelectorAll('img').forEach((button) => button.removeAttribute("class"));
 
           buttonInterval = setInterval(() => {
             if (
